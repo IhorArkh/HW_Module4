74a7950c36457e331e2a5ab5842ba99e32b2c999
Ihor Arkhypenko <wasbornindecember@gmail.com>
2023-02-12 18:51:50 +0200

    Relocating to another comp

--- HW_4.3_CreatingDB/Program.cs
+++ HW_4.3_CreatingDB/Program.cs
@@ -4,7 +4,8 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var file = File.ReadAllText(@"C:\Users\Biostar\source\repos\HW_Module4\HW_4.3_CreatingDB\appsettings.json");
+            
         }
     }
 }
