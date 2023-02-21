793b6ebbeb4096a6d806299aa7e72ab1d4443afa
Ihor Arkhypenko <wasbornindecember@gmail.com>
2023-02-21 15:51:01 +0200

    New project for developing created

---
+++ HW_4.6_Module/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace HW_4._6_Module
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
